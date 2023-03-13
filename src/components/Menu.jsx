9be66443a0813512854d049ecdf63b8dbab0dd84
PELUMI <folamioluwapelumi1@gmail.com>
2023-03-14 00:15:07 +0100

    each-page redirect

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { urlFor } from "../../library/client";
 import css from "../styles/Menu.module.css";
+import Link from "next/link";
 
 export const Menu = ({ icecreams }) => {
   console.log(icecreams);
@@ -14,22 +15,31 @@ export const Menu = ({ icecreams }) => {
       </div>
 
       {/* menu */}
-      {icecreams.map((icecream, id) => {
-        const src = urlFor(icecream.image).url();
-        return (
-          <div className={css.icecreamMenu} key={id}>
-            <div className={css.icecreamImage}>
-              <Image
-                loader={() => src}
-                src={src}
-                alt=""
-                objectFit="cover"
-                layout="fill"
-              />
+      <div className={css.iceMenu}>
+        {icecreams.map((icecream, id) => {
+          const src = urlFor(icecream.image).url();
+          return (
+            <div className={css.icecreamMenu} key={id}>
+              <Link href={`./icecream/${icecream.slug.current}`}>
+                <div className={css.icecreamImage}>
+                  <Image
+                    loader={() => src}
+                    src={src}
+                    alt=""
+                    objectFit="cover"
+                    layout="fill"
+                  />
+                </div>
+                <span className={css.iceName}>{icecream.name}</span>
+                <span className={css.icePrice}>
+                  <span style={{ color: "var(--hoverColor)" }}> ₦ </span>
+                  {icecream.price[1]}
+                </span>
+              </Link>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
