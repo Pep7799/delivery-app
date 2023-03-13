61cf3db46ea9f6f3f3698d57f8cfa5b64461e0de
PELUMI <folamioluwapelumi1@gmail.com>
2023-03-13 17:06:07 +0100

    'images'

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import React from "react";
+import { urlFor } from "../../library/client";
 import css from "../styles/Menu.module.css";
 
 export const Menu = ({ icecreams }) => {
@@ -10,6 +12,24 @@ export const Menu = ({ icecreams }) => {
         <span>Check out our menu </span>
         <span>and get it in a minute! </span>
       </div>
+
+      {/* menu */}
+      {icecreams.map((icecream, id) => {
+        const src = urlFor(icecream.image).url();
+        return (
+          <div className={css.icecreamMenu} key={id}>
+            <div className={css.icecreamImage}>
+              <Image
+                loader={() => src}
+                src={src}
+                alt=""
+                objectFit="cover"
+                layout="fill"
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
